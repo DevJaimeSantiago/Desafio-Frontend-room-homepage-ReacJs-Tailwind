7eea0505595ab7d3acd4b2c51b3a6f4a82f8430f
DevJaimeSantiago <jaimestgo475@gmail.com>
2022-11-17 14:14:52 -0600

    sesion 2

---
+++ src/components/Icon/IconNext.jsx
@@ -0,0 +1,18 @@
+const IconNext = (props) => {
+  return (
+    <svg
+      width="14"
+      height="24"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+      >
+        <path
+          d="M1 0l12 12L1 24"
+          stroke="#FFF"
+          fill="none"
+        />
+    </svg>
+  )
+}
+
+export default IconNext
